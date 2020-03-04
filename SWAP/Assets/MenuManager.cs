38bfac380fcbc6956e6c0229c258b23525128d6a
JunRong19 <tanjunrong321@gmail.com>
2020-03-04 21:51:05 +0800

    Added settings page
    - Imported UI pack

--- SWAP/Assets/MenuManager.cs
+++ SWAP/Assets/MenuManager.cs
@@ -3,4 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour {
+
+	[SerializeField, Tooltip("Reference to settings canvas")] private GameObject settingsCanvas;
+
+	public void ToggleSettingPage(bool state) {
+		settingsCanvas.SetActive(state);
+	}
 }
